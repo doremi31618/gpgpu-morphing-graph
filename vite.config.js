869591233aeda1efecba74e473ec47eb9881ed22
doremi31618 <doremi31618@gmail.com>
2024-03-21 21:45:31 +0800

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 export default defineConfig({
-    plugins: [glsl()]
+    plugins: [glsl()],
+    base: '/gpgpu-morphing-graph/'
   });
